c9c8a46aee4146b93faf2947bb7b901f6917affd
hajoubi <hajoubi.mohammed2@gmail.com>
2024-04-12 01:08:42 +0000

    keylloger

---
+++ dofus bot/README.md
@@ -0,0 +1,10 @@
+hello, my name is mohammed i created this project so that i can see how far i can go with automation skills in python.
+Let's get to it, my goal is to be able to automate harder bosses and farm easily.
+i want to open source it so that anyone can contribute and help people.
+tools: 
+	keylogger is useful to monitor your click and button for later use
+Modules used:
+	.pynput
+	.pyautogui
+
+
---
+++ dofus bot/keyloager.py
@@ -0,0 +1,45 @@
+import pynput
+from pynput.mouse import Listener
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        with open("alpha.txt", "a") as f:
+            f.write((f"{key.char}\n"))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+    
+        
+
+def on_click(x, y, button, pressed):
+   if pressed:
+       with open("click_coordinates.txt", "a") as f:
+           f.write(f"{x}, {y}\n")
+
+
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+with Listener(on_click=on_click) as listener:
+   listener.join()
+
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
